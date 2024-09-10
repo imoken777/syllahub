67b30234a7f2e26950a9cdd039c29635bab4f853
imoken777 <imoken27@gmail.com>
2024-09-11 02:47:27 +0900

    fix: デフォルトのスタイルをlintignore

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,6 +10,7 @@ const NotFound = () => (
             __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
           }}
         />
+        {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
         <h1 className="next-error-h1" style={styles.h1}>
           404
         </h1>
