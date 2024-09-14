eae6bd8f98f4f186d183bba14a8c95d618f07cfc
imoken777 <imoken27@gmail.com>
2024-09-14 22:55:38 +0900

    feat: parserも調整

--- src/app/api/[...route]/course/parser.ts
+++ src/app/api/[...route]/course/parser.ts
@@ -19,11 +19,11 @@ export const parseSemester = (text: string): string => {
  * 出力例: ['金', '3']
  *
  * @param {string} text - 日本語と英語で書かれた曜日と時限の情報
- * @returns {[string | undefined, string | undefined]} - 曜日と時限を配列で返す [day, period]
+ * @returns {[string | null, string | null]} - 曜日と時限を配列で返す [day, period]
  */
-export const parseDayAndPeriod = (text: string): [string | undefined, string | undefined] => {
+export const parseDayAndPeriod = (text: string): [string | null, string | null] => {
   const match = text.match(/(.+),.+限.+,(\d+)/);
-  return [match?.[1].trim(), match?.[2].trim()];
+  return [match?.[1] ?? null, match?.[2] ?? null];
 };
 
 /**
@@ -33,11 +33,9 @@ export const parseDayAndPeriod = (text: string): [string | undefined, string | u
  * 出力例: { startYear: 1, endYear: 4 }
  *
  * @param {string} text - 範囲を表す年次のテキスト
- * @returns {{ startYear: number; endYear: number } | undefined} - 開始年次と終了年次のオブジェクト、もしくは範囲が不正な場合は undefined
+ * @returns {{ startYear: number; endYear: number } | null} - 開始年次と終了年次のオブジェクト、もしくは範囲が不正な場合は null
  */
-export const parseYearOfStudy = (
-  text: string,
-): { startYear: number; endYear: number } | undefined => {
+export const parseYearOfStudy = (text: string): { startYear: number; endYear: number } | null => {
   const match = text.match(/(\d+)(?:〜(\d+))?/);
 
   if (match) {
@@ -49,7 +47,7 @@ export const parseYearOfStudy = (
       endYear,
     };
   }
-  return undefined;
+  return null;
 };
 
 /**
@@ -59,16 +57,17 @@ export const parseYearOfStudy = (
  * 出力例: URL('https://g-sys.toyo.ac.jp/syllabus/html/gakugai/2024/2024_156008.html?numbering=SUS101')
  *
  * @param {string | undefined} rawData - 正規表現で解析する元データ
- * @returns {URL | undefined} - シラバスのURLオブジェクト、もしくは解析できなかった場合は undefined
+ * @returns {string | null} - シラバスのリンクをstringで、もしくは解析できなかった場合は null
  */
-export const assembleSyllabusLink = (rawData: string | undefined): URL | undefined => {
-  if (!rawData) return undefined;
+export const assembleSyllabusLink = (rawData: string | undefined): string | null => {
+  if (!rawData) return null;
   const match = rawData.match(/showSbs\('(\d+)',\s*'(\d+)',\s*'(\w+)',\s*'(\w+)'\)/);
   if (match) {
     const [, year, code1, code2] = match;
-    return new URL(
-      `https://g-sys.toyo.ac.jp/syllabus/html/gakugai/${year}/${year}_${code1}.html?numbering=${code2}`,
-    );
+    // return new URL(
+    //   `https://g-sys.toyo.ac.jp/syllabus/html/gakugai/${year}/${year}_${code1}.html?numbering=${code2}`,
+    // );
+    return `https://g-sys.toyo.ac.jp/syllabus/html/gakugai/${year}/${year}_${code1}.html?numbering=${code2}`;
   }
-  return undefined;
+  return null;
 };
