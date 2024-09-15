8b80c09b677cd23f160271eff1f9758784ef126d
imoken777 <imoken27@gmail.com>
2024-09-15 21:37:55 +0900

    feat: 仮で`course/`api実装

--- src/app/api/[...route]/course/index.ts
+++ src/app/api/[...route]/course/index.ts
@@ -1,17 +1,13 @@
 import { courses } from '@/drizzle/schema';
 import { db } from '@/lib/db';
+import type { Course } from '@/types/course';
 import { honoFactory } from '../factory';
-import { getAllSyllabus } from './getAllSyllabus';
 
-export const courseRouter = honoFactory.createApp().put('/cron', async (c) => {
-  const data = await getAllSyllabus();
+export const courseRouter = honoFactory.createApp().get('/', async (c) => {
   try {
-    await db.delete(courses);
-    for (let i = 0; i < data.length; i += 10) {
-      await db.insert(courses).values(data.slice(i, i + 10));
-    }
+    const data: Course[] = await db.select().from(courses).all();
+    return c.json(data);
   } catch (e) {
     return c.json({ error: e }, 500);
   }
-  return c.json('OK', 200);
 });
