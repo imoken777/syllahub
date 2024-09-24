95783521beeee4b778ff359941a7c448c3fc36bb
imoken777 <imoken27@gmail.com>
2024-09-24 22:23:30 +0900

    feat: CourseListコンポーネント追加

---
+++ src/components/model/course/CourseList.tsx
@@ -0,0 +1,10 @@
+import type { CourseModel } from '@/types/course';
+import { CourseItem } from './CourseItem';
+
+export const CourseList = ({ courses }: { courses: CourseModel[] }) => (
+  <div className="space-y-4">
+    {courses.map((course, _) => (
+      <CourseItem key={course.courseId} course={course} />
+    ))}
+  </div>
+);
