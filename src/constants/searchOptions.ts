c2c42e9b3c2374c917c088dde9bde10d758bd030
imoken777 <imoken27@gmail.com>
2024-10-02 12:05:07 +0900

    feat: searchOptions定数を追加

---
+++ src/constants/searchOptions.ts
@@ -0,0 +1,9 @@
+export const semesterOptions = ['春学期', '秋学期'] as const;
+export const typeOfConductionOptions = [
+  '対面',
+  '非対面（同時双方向）',
+  '非対面（オンデマンド）',
+] as const;
+export const dayOptions = ['月', '火', '水', '木', '金'] as const;
+export const periodOptions = ['1限', '2限', '3限', '4限', '5限', '6限'] as const;
+export const languageOptions = ['日本語', '英語'] as const;
