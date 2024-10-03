48ac1f5e02b469bbc446295ff475ca8c21fc39d3
imoken777 <imoken27@gmail.com>
2024-10-03 20:49:26 +0900

    feat: shemaとcourse型を堅牢な型に

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,3 +1,10 @@
+import {
+  dayOptions,
+  languageOptions,
+  periodOptions,
+  semesterOptions,
+  typeOfConductionOptions,
+} from '@/constants/searchOptions';
 import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
@@ -5,17 +12,17 @@ import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 export const courses = sqliteTable('courses', {
   courseId: integer('courseId').primaryKey(),
   courseName: text('courseName').notNull(),
-  semester: text('semester').notNull(),
+  semester: text('semester', { enum: semesterOptions }).notNull(),
   groupName: text('groupName').notNull(),
   instructors: text('instructors', { mode: 'json' }).$type<string[]>().notNull(),
-  languageOptions: text('languageOptions').notNull(),
-  typeOfConduction: text('typeOfConduction').notNull(),
+  languageOptions: text('languageOptions', { enum: languageOptions }).notNull(),
+  typeOfConduction: text('typeOfConduction', { enum: typeOfConductionOptions }).notNull(),
   yearOfStudy: text('yearOfStudy', { mode: 'json' })
     .$type<{ startYear: number; endYear: number }>()
     .default(sql`null`),
   syllabusLink: text('syllabusLink').default(sql`null`),
-  day: text('day'),
-  period: text('period'),
+  day: text('day', { enum: dayOptions }),
+  period: text('period', { enum: periodOptions }),
 });
 
 export const insertCourseSchema = createInsertSchema(courses);
--- src/types/course.ts
+++ src/types/course.ts
@@ -1,18 +1,20 @@
+import type { Day, LanguageOptions, Period, Semester, TypeOfConduction } from './searchOptions';
+
 export type CreateCourseDto = {
   courseName: string;
-  semester: string;
+  semester: Semester;
   groupName: string;
   instructors: string[];
-  languageOptions: string;
-  typeOfConduction: string;
+  languageOptions: LanguageOptions;
+  typeOfConduction: TypeOfConduction;
   yearOfStudy: {
     startYear: number;
     endYear: number;
   } | null;
   //   syllabusLink: URL | null;
   syllabusLink: string | null;
-  day: string | null;
-  period: string | null;
+  day: Day | null;
+  period: Period | null;
 };
 
 export type CourseModel = CreateCourseDto & { courseId: number };
