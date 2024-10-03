4d62781d12688748a080b775583c4c7f31143c73
imoken777 <imoken27@gmail.com>
2024-10-03 20:51:14 +0900

    feat: course取得時にバリデーションするように

--- src/app/api/[...route]/internal/course/index.ts
+++ src/app/api/[...route]/internal/course/index.ts
@@ -1,12 +1,15 @@
-import { courses } from '@/drizzle/schema';
+import { courses, selectCourseSchema } from '@/drizzle/schema';
 import { db } from '@/lib/db';
 import type { CourseModel } from '@/types/course';
 import { internalFactory } from '../factory';
 
 export const courseRouter = internalFactory.createApp().get('/', async (c) => {
   try {
-    const data: CourseModel[] = await db.select().from(courses).all();
-    return c.json(data);
+    const data = await db.select().from(courses).all();
+    const validData: CourseModel[] = data.filter(
+      (course) => selectCourseSchema.safeParse(course).success,
+    );
+    return c.json(validData, 200);
   } catch (e) {
     return c.json({ error: e }, 500);
   }
