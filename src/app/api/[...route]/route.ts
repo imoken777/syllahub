25e3a34c7466d7e8e18946ecf04f0483861e64c0
imoken777 <imoken27@gmail.com>
2024-10-21 13:08:05 +0900

    refactor: 不要なapiを削除

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -1,16 +1,9 @@
 import { handle } from 'hono/vercel';
-import { courseRouter } from './internal/course';
-import { internalFactory } from './internal/factory';
 import { cronRouter } from './public/cron';
 import { publicFactory } from './public/factory';
 
 export const runtime = 'edge';
 const basePath = '/api';
-
-const internalApp = internalFactory.createApp().basePath(basePath);
-const internalRoutes = internalApp.route('/course', courseRouter);
-export const GET = handle(internalRoutes);
-export type APIType = typeof internalRoutes;
 
 const publicApp = publicFactory.createApp().basePath(basePath);
 const publicRoutes = publicApp.route('/cron', cronRouter);
--- src/lib/apiClient.ts
+++
@@ -1,5 +0,0 @@
-import type { APIType } from '@/app/api/[...route]/route';
-import { hc } from 'hono/client';
-
-const baseUrl = new URL('https://sillahub.pages.dev/');
-export const apiClient = hc<APIType>(baseUrl.href);
