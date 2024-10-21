f3b481a00c0dcca9008d8949bcbc96297780445f
imoken777 <imoken27@gmail.com>
2024-10-21 13:09:20 +0900

    refactor: サーバーコンポーネントから直接dbアクセスするように修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { IndexContainer } from '@/components/layout/IndexContainer';
-import { apiClient } from '@/lib/apiClient';
+import { courses } from '@/drizzle/schema';
+import { db } from '@/lib/db';
 import type { CourseModel } from '@/types/course';
 
+export const runtime = 'edge';
+
 const Home = async () => {
-  const coursesRes = await apiClient.api.course.$get();
-  if (!coursesRes.ok) return <div>データの取得に失敗しました</div>;
-  const courses: CourseModel[] = await coursesRes.json();
+  const courseData: CourseModel[] = await db.select().from(courses).all();
+  if (courseData.length === 0) return <div>データがありません</div>;
 
   return (
     <main>
-      <IndexContainer courses={courses} />
+      <IndexContainer courses={courseData} />
     </main>
   );
 };
