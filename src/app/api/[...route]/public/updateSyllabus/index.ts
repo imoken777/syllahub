b2d551e86dd307673eaeb8d6b896f24ea7fa0cad
imoken777 <imoken27@gmail.com>
2025-05-26 04:14:33 +0900

    fix: シラバス更新操作をトランザクション内で行うように変更

--- src/app/api/[...route]/public/updateSyllabus/index.ts
+++ src/app/api/[...route]/public/updateSyllabus/index.ts
@@ -6,10 +6,10 @@ import { getAllSyllabus } from './getAllSyllabus';
 export const updateSyllabusRouter = publicFactory.createApp().put('/', async (c) => {
   const data = await getAllSyllabus();
   try {
-    await db.delete(courses);
-    for (let i = 0; i < data.length; i += 10) {
-      await db.insert(courses).values(data.slice(i, i + 10));
-    }
+    await db.transaction(async (tx) => {
+      await tx.delete(courses);
+      await tx.insert(courses).values(data);
+    });
     return c.json('OK', 200);
   } catch (e) {
     return c.json({ error: e }, 500);
