e34208194486ac528a72a5b91b0959993ba1165e
imoken777 <imoken27@gmail.com>
2025-06-30 19:41:31 +0900

    chore: `next.config.mjs`のexperimental設定を追加し、画像フォーマットとキャッシュTTLを設定

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,23 @@ initOpenNextCloudflareForDev();
 
 const nextConfig = {
   serverExternalPackages: ['@libsql/isomorphic-ws'],
+  swcMinify: true,
+  experimental: {
+    optimizePackageImports: ['lucide-react', '@radix-ui/react-select', '@radix-ui/react-popover'],
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+
+  images: {
+    formats: ['image/webp', 'image/avif'],
+    minimumCacheTTL: 31536000, // 1年
+  },
 };
 
 export default nextConfig;
