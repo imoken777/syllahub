b32741786b7e14fa72ca809dd5b96f3b0a65f45a
imoken777 <imoken27@gmail.com>
2025-06-30 16:57:36 +0900

    fix: scheduledハンドラーをオブジェクトとして正しくexportするように

--- src/workers/scheduled.ts
+++ src/workers/scheduled.ts
@@ -1,12 +1,15 @@
 import { updateSyllabusService } from '@/services/syllabus';
+import type { Env } from 'hono/types';
 
-const scheduled: ExportedHandlerScheduledHandler = async (event, env, ctx) => {
-  ctx.waitUntil(
-    updateSyllabusService()
-      // eslint-disable-next-line no-console
-      .then((res) => console.log('Syllabus update completed:', res))
-      .catch((err) => console.error('Error during syllabus update:', err)),
-  );
+const scheduledWorker = {
+  async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext) {
+    ctx.waitUntil(
+      updateSyllabusService()
+        // eslint-disable-next-line no-console
+        .then((res) => console.log('Syllabus update completed:', res))
+        .catch((err) => console.error('Error during syllabus update:', err)),
+    );
+  },
 };
 
-export default scheduled;
+export default scheduledWorker;
