b26af3d58f0be468c9daf04a711d9b41748b807c
imoken777 <imoken27@gmail.com>
2025-08-08 23:26:54 +0900

    fix: 仮PUTメソッドを追加し、無効なAPIレスポンスを返す

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -6,3 +6,8 @@
 // const publicApp = publicFactory.createApp().basePath(basePath);
 // const publicRoutes = publicApp.route('/updateSyllabus', updateSyllabusRouter);
 // export const PUT = handle(publicRoutes);
+
+// 仮
+export async function PUT() {
+  return new Response('This API is disabled', { status: 404 });
+}
